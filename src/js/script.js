e221e753d48f3206b5994e6cb183742081e582ad
Roman <asdwka3@gmail.com>
2023-11-12 19:02:23 +0100

    skill section

--- src/js/script.js
+++ src/js/script.js
@@ -10,3 +10,9 @@ closeElem.addEventListener('click', function() {
     menuButton.classList.toggle('active');
 });
 
+const counters = document.querySelectorAll('.skills__ratings-counter'); //чтобы обратиться ко всем элементам используем all
+const lines = document.querySelectorAll('.skills__ratings-line span');
+
+counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
